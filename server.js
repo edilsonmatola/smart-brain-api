ca7d70e645776ff0941575480bf63c792d292412
Edilson Matola <edilsonmatola13@gmail.com>
2022-01-27 11:34:28 +0200

    chore: Added Routes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
 
 // Variable for database
 const database = {
@@ -26,9 +29,9 @@ const database = {
 
 // *  ==============   Routes  =================
 
-//* ==== Home route ==========
+//* ==== Root route ==========
 app.get("/", (req, res) => {
-  res.send("This is working");
+  res.send(database.users);
 });
 
 // * ====== Signin Route ==============
@@ -39,10 +42,57 @@ app.post("/signin", (req, res) => {
   ) {
     res.json("success");
   } else {
-    res.status(400).json("error loggin in");
+    res.status(404).json("error loggin in");
   }
 });
 
+// * ====== Register Route ==============
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "124",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+// * ====== Profile Route ==============
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("Not found");
+  }
+});
+
+// * ====== image Route ==============
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("Not found");
+  }
+});
+
+// Server port
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
